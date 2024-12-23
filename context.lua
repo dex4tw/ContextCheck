f6e1492793581b96b7fe413a8db22a575c2f7704
dex <121840104+dex4tw@users.noreply.github.com>
2024-12-23 18:22:58 -0500

    small

--- context.lua
+++ context.lua
@@ -1,20 +1,19 @@
--- Member Tags: None, LocalUserSecurity, PluginSecurity, RobloxScriptSecurity, NotAccessibleSecurity, RobloxSecurity
-
+local Identity
 local Security = {
-	{"None", function() return workspace.Name end},
-	{"LocalUserSecurity", function() return workspace.DataCost end},
-	{"PluginSecurity", function() return workspace.RobloxLocked end},
-	{"RobloxScriptSecurity", function() return game:GetService("CoreGui").SelectionImageObject end},
-	{"NotAccessibleSecurity", function() game:GetService("Chat").LoadDefaultChat = game:GetService("Chat").LoadDefaultChat end},
-	{"RobloxSecurity", function() return game:GetService("CSGDictionaryService").Name end},
+    {"0None", function() return workspace.Name end},
+    {"3LocalUserSecurity", function() return workspace.DataCost end},
+    {"1PluginSecurity", function() return workspace.RobloxLocked end},
+    {"3RobloxScriptSecurity", function() return game:GetService("CoreGui").SelectionImageObject end}, -- CoreScript
+    {"0NotAccessibleSecurity", function() game:GetService("Chat").LoadDefaultChat = game:GetService("Chat").LoadDefaultChat end},
+    {"6RobloxSecurity", function() return game:GetService("CSGDictionaryService").Name end}    
 }
 
 for _, Tag in ipairs(Security) do
-	local Name, Test = Tag[1], Tag[2]
-	local CanDo, Error = pcall(Test)
-	if CanDo then
-		printidentity("Check passed for tag "..Name.." for identity")
-	else
-		printidentity("[Permission "..Error:sub(-2, -2).."] Check failed for tag "..Name.." for identity")
-	end
+    local Name, Test = Tag[1], Tag[2]
+    local Success, Exception = pcall(Test)
+    if Success then
+        Identity = Name:sub(1,1)
+    end
 end
+
+print("Exploit Authentic Identity:", tostring(Identity))
