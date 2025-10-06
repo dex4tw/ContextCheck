f3ebb31c459c90b58fa0c4ef2a6b3b5bfe13d286
dex <121840104+dex4tw@users.noreply.github.com>
2025-10-05 23:50:31 -0400

    Update context.lua

--- context.lua
+++ context.lua
@@ -1,9 +1,16 @@
+--[[
+    ContextCheck
+        I <3 External Executors
+        p.s. Externals usually fall @ Level 3 (RobloxScriptSecurity)
+    ~ @dexftl . https://github.com/dex4tw
+]]--
+
 local Identity
 local Security = {
     [0] = {"None", function() return workspace.Name end},
     [3] = {"LocalUserSecurity", function() return workspace.DataCost end},
     [1] = {"PluginSecurity", function() return workspace.RobloxLocked end},
-    [3] = {"RobloxScriptSecurity", function() return game:GetService("CoreGui").SelectionImageObject end}, -- CoreScript
+    [3] = {"RobloxScriptSecurity", function() return game:GetService("CoreGui") end},
     [0] = {"NotAccessibleSecurity", function() game:GetService("Chat").LoadDefaultChat = game:GetService("Chat").LoadDefaultChat end},
     [6] = {"RobloxSecurity", function() return game:GetService("CSGDictionaryService").Name end}    
 }
@@ -12,7 +19,7 @@ for _, Tag in ipairs(Security) do
     local Name, Test = Tag[1], Tag[2]
     local Success, Exception = pcall(Test)
     if Success then
-        Identity = tostring(_) .. "(" .. Tag[1] .. ")"
+        Identity = tostring(_) .. " (" .. Tag[1] .. ")"
     end
 end
 
