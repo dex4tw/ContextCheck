1bfc3f6b69ff248fa7a3962f51faf9ee4a0896eb
dex <121840104+dex4tw@users.noreply.github.com>
2025-10-05 23:46:44 -0400

    Update context.lua

--- context.lua
+++ context.lua
@@ -1,18 +1,18 @@
 local Identity
 local Security = {
-    {"0None", function() return workspace.Name end},
-    {"3LocalUserSecurity", function() return workspace.DataCost end},
-    {"1PluginSecurity", function() return workspace.RobloxLocked end},
-    {"3RobloxScriptSecurity", function() return game:GetService("CoreGui").SelectionImageObject end}, -- CoreScript
-    {"0NotAccessibleSecurity", function() game:GetService("Chat").LoadDefaultChat = game:GetService("Chat").LoadDefaultChat end},
-    {"6RobloxSecurity", function() return game:GetService("CSGDictionaryService").Name end}    
+    [0] = {"None", function() return workspace.Name end},
+    [3] = {"LocalUserSecurity", function() return workspace.DataCost end},
+    [1] = {"PluginSecurity", function() return workspace.RobloxLocked end},
+    [3] = {"RobloxScriptSecurity", function() return game:GetService("CoreGui").SelectionImageObject end}, -- CoreScript
+    [0] = {"NotAccessibleSecurity", function() game:GetService("Chat").LoadDefaultChat = game:GetService("Chat").LoadDefaultChat end},
+    [6] = {"RobloxSecurity", function() return game:GetService("CSGDictionaryService").Name end}    
 }
 
 for _, Tag in ipairs(Security) do
     local Name, Test = Tag[1], Tag[2]
     local Success, Exception = pcall(Test)
     if Success then
-        Identity = Name:sub(1,1)
+        Identity = tostring(_) .. "(" .. Tag[1] .. ")"
     end
 end
 
